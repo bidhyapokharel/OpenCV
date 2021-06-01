044979325b22492e38e884d321bdf9a47de5b903
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-06-01 12:03:18 +0545

    Write image

--- read_image.py
+++ read_image.py
@@ -3,7 +3,11 @@ import cv2
 img = cv2.imread('lena.jpg', 0)
 
 cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-print(img)
+k = cv2.waitKey(0)
+if k == 27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('lena_black.jpg',img)
+    cv2.destroyAllWindows()
+else:
+   cv2.destroyAllWindows() 
