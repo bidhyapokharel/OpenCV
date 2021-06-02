3fe5a8bb321a314e27b8ae48806ed663c8870925
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-06-02 11:01:23 +0545

    Add date and time4

---
+++ numpy_for_image.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+# img = cv2.imread('lena.jpg', 0)
+img = np.zeros([512,512,3], np.uint8)
+
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('lena_black.jpg',img)
+    cv2.destroyAllWindows()
+else:
+   cv2.destroyAllWindows() 
