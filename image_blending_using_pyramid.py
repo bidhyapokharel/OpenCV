e2c82e108750fe52c3a428ce622ac8b80b43bccc
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-06-06 15:08:12 +0545

    Find and Draw Contours

--- image_blending_using_pyramid.py
+++ image_blending_using_pyramid.py
@@ -32,8 +32,8 @@ for i in range(5,0,-1):
     cv2.imshow(str(i), lap_lassian)
 
 # Laplasian for Orange
-orange_copy = qp_apple[5]
-lp_orange = [apple_copy]
+orange_copy = qp_orange[5]
+lp_orange = [orange_copy]
 for i in range(5,0,-1):
     gaussian_extended = cv2.pyrUp(qp_orange[i])
     lap_lassian = cv2.subtract(qp_orange[i-1], gaussian_extended)
@@ -44,22 +44,21 @@ apple_orange_pyramid = []
 n = 0
 for apple_lap, orange_lap in zip(lp_apple, lp_orange):
     n += 1
+    print(apple_lap.shape)
+    print(orange_lap.shape)
     cols, rows, ch = apple_lap.shape
-    laplacian = np.hstack(apple_lap[:, 0:int(cols/2)], orange_lap[:, int(cols/2):])
+    laplacian = np.hstack((apple_lap[:, 0:int(cols/2)], orange_lap[:, int(cols/2):]))
     apple_orange_pyramid.append(laplacian)
 
 # Now reconstruct
 apple_orange_reconstruct = apple_orange_pyramid[0]
-for i in range(1,6):
+for i in range(1,5):
     apple_orange_reconstruct = cv2.pyrUp(apple_orange_reconstruct)
     apple_orange_reconstruct = cv2.add(apple_orange_pyramid[i], apple_orange_reconstruct)
-
-
 
 cv2.imshow('Apple', apple)
 cv2.imshow('Orange', orange)
 cv2.imshow('apple_orange_reconstruct', apple_orange_reconstruct )
-
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
