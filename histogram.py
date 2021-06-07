758c45152230ab787843b31fa725014898145425
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-06-07 11:20:51 +0545

    Histogram

---
+++ histogram.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('lena.jpg')
+# b,g,r = cv2.split(img)
+
+# cv2.imshow("img", img)
+# cv2.imshow("b", b)
+# cv2.imshow("g", g)
+# cv2.imshow("r", r)
+
+hist = cv2.calcHist([img], [0], None, [256], [0,256])
+plt.plot(hist)
+# plt.hist(img.ravel(), 256, [0,256])
+# plt.hist(b.ravel(), 256, [0,356])
+# plt.hist(g.ravel(), 256, [0,356])
+# plt.hist(r.ravel(), 256, [0,356])
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
